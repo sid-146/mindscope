e62c4cea15aa5856cea88864ee1da8973a591afe
Sudhanwa Kaveeshwar <65759652+sid-146@users.noreply.github.com>
2025-08-20 12:32:54 +0530

    Update base.py

--- mindscope/components/llm/base.py
+++ mindscope/components/llm/base.py
@@ -1,1 +1,18 @@
+from abc import ABC, abstractmethod
 
+
+class BaseLLM(ABC):
+    def __init__(self):
+        return
+
+    @abstractmethod
+    def chat():
+        raise NotImplementedError("Base class should implement this method.")
+
+    @abstractmethod
+    def embed():
+        raise NotImplementedError("Base class should implement this method.")
+
+    @abstractmethod
+    def generate_text():
+        raise NotImplementedError("Base class should implement this method.")
