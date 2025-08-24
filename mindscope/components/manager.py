0f66eed1f4f4d7b338caf28ff81fefaeb3498f1e
Sudhanwa <sudhanwa.kaveeshwar@gmail.com>
2025-08-24 21:02:01 +0530

    Moved function to utils.

--- mindscope/components/manager.py
+++ mindscope/components/manager.py
@@ -4,50 +4,12 @@ Todo:
     - move non class functions to their correct position
 """
 
-import os
-from typing import Callable, Dict, Hashable
-
 import polars as pl
 
-# types
-LoaderDict = Dict[Hashable, Callable[[], pl.DataFrame]]
-EMPTY_DF = pl.DataFrame()
-
+from .core import EMPTY_DF
+from .utils.manager import get_dataframe_from_filepath
 
 # Errors
-class FileReadError(BaseException):
-    pass
-
-
-def get_dataframe_from_filepath(filepath: str, encoding: str = "utf-8"):
-    if not os.path.exists(filepath):
-        raise FileNotFoundError("Given file not found..")
-
-    extn = filepath.split(".")[-1]
-    mapping: LoaderDict = {
-        "csv": lambda: pl.read_csv(filepath, encoding=encoding),
-        "xlsx": lambda: pl.read_excel(filepath, encoding=encoding),
-        "xls": lambda: pl.read_excel(filepath, encoding=encoding),
-        "parquet": lambda: pl.read_parquet(filepath),
-        "json": lambda: pl.read_json(filepath, orient="records", encoding=encoding),
-    }
-
-    if extn not in mapping:
-        raise KeyError(
-            f"Given extension {extn} not in mapping \
-            available extensions are : {list(mapping.keys())}"
-        )
-
-    try:
-        df: pl.DataFrame = mapping[extn]()
-    except Exception as e:
-        message = f"Not able to read file : {e.__class__} : {e}"
-        raise FileReadError(message)
-
-    if df.empty:
-        raise pl.errors.EmptyDataError("File loaded but no data found.")
-
-    return df
 
 
 class Manager:
