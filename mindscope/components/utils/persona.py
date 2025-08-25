463e8b27dc2a3f268236bd3abaab6243b948ffb8
Sudhanwa Kaveeshwar <65759652+sid-146@users.noreply.github.com>
2025-08-25 13:01:33 +0530

    Update persona.py

--- mindscope/components/utils/persona.py
+++ mindscope/components/utils/persona.py
@@ -1,1 +1,11 @@
+import json
+import os
 
+
+def from_json(path: str):
+    if os.path.exists(path):
+        with open(path, "r") as f:
+            persona = json.load(f)
+        return persona
+    else:
+        raise FileNotFoundError("No file found at given path")
