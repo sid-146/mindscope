92c7fbe8e49b42a65708feebebeb5f070dfa34b4
Sudhanwa Kaveeshwar <65759652+sid-146@users.noreply.github.com>
2025-08-26 11:40:25 +0530

    Update persona.py
    - Now persona name is mandatory

--- mindscope/components/persona.py
+++ mindscope/components/persona.py
@@ -1,11 +1,11 @@
 from core import FileNotSupportedError
 from typing import List, Dict
-from .persona_helper import from_json
+from persona_helper import from_json
 
 
 class Persona:
-    def __init__(self):
-        self._name: str = None
+    def __init__(self, name: str):
+        self._name: str = name
         self._description: str = None
         self._goals: List[str] = None
         self._pain_points: List[str] = None
@@ -69,7 +69,11 @@ class Persona:
             raise FileNotSupportedError("Given file format is not supported.")
         try:
             persona = Persona()
-            persona.name = data.get("name")
+            if name := data.get("name"):
+                persona.name = name
+            else:
+                raise AttributeError("Persona Should be initialized with the name.")
+
             persona.description = data.get("description")
             persona.goals = data.get("goals")
             persona.pain_points = data.get("pain_points")
