5d2fb9353826ece319ad00083064c17ca2d5c507
Sudhanwa <sudhanwa.kaveeshwar@gmail.com>
2025-09-01 20:52:24 +0530

    update openai.py

--- mindscope/components/metrics.py
+++ mindscope/components/metrics.py
@@ -2,6 +2,7 @@
 File to generate metrics according to persona.
 """
 
+import json
 from typing import Dict, List
 
 from .models import MetricModel
@@ -34,17 +35,35 @@ class MetricsHandler:
         """
         Generate metrics according to persona, and summary
         """
-        # Todo: Call llm and ask to generate given number of metric, for summary and persona
+        # Todo: Create prompt using summary, persona and no_of_metrics
         llm_client = llm(provider="openai")
         prompt = []
+        # generating using default generation config
+        response = llm_client.generate_text(prompt, MetricModel)
+        # try to parse returned json string to MetricModel
+        try:
+            content = response.text[0].content
+            content = json.loads(content)
+            metrics = MetricModel(**content)
+            self._metrics = metrics
+        except Exception as e:
+            raise e
 
-        return
+        return metrics
 
     def update_definition(self, metric_name: str, instruction: str):
+        raise NotImplementedError("Not Implemented yet.")
         if metric_name not in self._metrics:
             raise KeyError("Given metric not found in the current metric list.")
 
-        metric: MetricModel = self._metrics[metric_name]
+        # metric: MetricModel = self._metrics[metric_name]
 
         # Todo: Call llm and ask to update metric using given instruction
         return
+
+    def remove_metric(self, metric_name: str):
+        raise NotImplementedError("Not Implemented yet.")
+
+    def iter_metrics(self):
+        for metric in self._metrics.values():
+            yield metric
